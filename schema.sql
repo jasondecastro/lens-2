171efdcb0355380fd6caef3f1aaa4e89aea6243d
jasondecastro <jasonrdecastro@gmail.com>
2015-11-28 04:31:04 -0500

    updated SQL stuff

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.10 (x86_64)
 --
--- Host: localhost    Database: development
+-- Host: localhost    Database: roadbeam
 -- ------------------------------------------------------
--- Server version 5.6.24
+-- Server version	5.6.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,11 +23,33 @@ DROP TABLE IF EXISTS `followers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `followers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `follower_username` varchar(120) DEFAULT NULL,
   `followed_username` varchar(120) DEFAULT NULL,
   `follower_id` int(11) DEFAULT NULL,
-  `followed_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `followed_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(120) DEFAULT NULL,
+  `last_name` varchar(120) DEFAULT NULL,
+  `poster_username` varchar(120) DEFAULT NULL,
+  `poster_id` int(11) DEFAULT NULL,
+  `text_post` varchar(340) DEFAULT NULL,
+  `image_post` varchar(340) DEFAULT NULL,
+  `video_post` varchar(340) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +67,7 @@ CREATE TABLE `upload` (
   `title` varchar(120) DEFAULT NULL,
   `description` varchar(120) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,19 +84,22 @@ CREATE TABLE `users` (
   `username` varchar(100) DEFAULT NULL,
   `email` varchar(120) NOT NULL,
   `pwdhash` varchar(100) NOT NULL,
+  `verification_code` varchar(7) DEFAULT NULL,
   `figure` varchar(120) DEFAULT NULL,
   `twitter` varchar(120) DEFAULT NULL,
   `instagram` varchar(120) DEFAULT NULL,
   `github` varchar(120) DEFAULT NULL,
   `location` varchar(120) DEFAULT NULL,
-  `bio` varchar(120) DEFAULT NULL,
+  `bio` varchar(256) DEFAULT NULL,
   `followers` int(11) DEFAULT NULL,
   `following` int(11) DEFAULT NULL,
   `appreciations` int(11) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `cover_picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -86,4 +111,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-26  2:00:21
+-- Dump completed on 2015-11-28  4:29:22
--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,7 @@ app.jinja_env.globals.update(randint=random.randint)
  
 app.secret_key = 'development key'
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/roadbeam'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/lens'
  
 from models import db
 db.init_app(app)
@@ -20,8 +20,8 @@ from flask.ext.mail import Mail, Message
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'web@aoiths.org'
-app.config['MAIL_PASSWORD'] = 'aoitweb!'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 
